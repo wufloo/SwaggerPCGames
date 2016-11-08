308c4c1ab45b25b5742b7fd57a18e27fe63e93a3
janrepress <janre.press@khk.ee>
2016-11-08 15:06:06 +0200

    API

--- app/Controller.php
+++ app/Controller.php
@@ -17,15 +17,9 @@ class Controller
         $pdo = new \PDO('mysql:host=' . DATABASE_HOSTNAME . ';dbname=' . DATABASE_DATABASE, DATABASE_USERNAME,
             DATABASE_PASSWORD);//v'ljakutsumine
         $this->db = new FluentPDO($pdo);
-        $this->pdo = $pdo;
 
     }
-    function get_colum_names(){
-        $q = $this->pdo->prepare("DESCRIBE tablename");
-        $q->execute();
-        $table_fields = $q->fetchAll(PDO::FETCH_COLUMN);
-        var_dump($table_fields);
-    }
+
     function output(Array $data){
         echo json_encode($data, JSON_PRETTY_PRINT);
     }
--- app/controllers/games.php
+++ app/controllers/games.php
@@ -16,13 +16,16 @@ class games extends Controller
     function get($parameters)
     {
         // Ensure that name exists
-        $games = empty($_GET['name']) ? '' : $_GET['name'];
+       // $games = empty($_GET['name']) ? '' : $_GET['name'];
 
         // Split pipe separated list of games into array
-        $games = explode('|', $games);
+      //  $games = explode('|', $games);
 
         // Retrieve requested games from database
-        $games = $this->db->from("game")->where("name", $games)->fetchAll();
+       // $games = $this->db->from("game")->where("name", $games)->fetchAll();
+
+
+        $games = $this->db->from("game")->fetchAll();
 
         // Output json encoded data
         $this->output($games);
@@ -33,40 +36,69 @@ class games extends Controller
     function post()
     {
         //$_POST['kala']='ahven';
-        $result = $this->db->insertInto('game', $_POST)->execute();
+        $json = file_get_contents('php://input');
+        $game = json_decode($json, 1);
+        $result = $this->db->insertInto('game', $game)->execute();
         $this->output((array)$result);
 
     }
 
     function put()
     {
+
         // Convert json encoded request body into object
         $request = json_decode(file_get_contents('php://input'));
 
-        // Convert object to array
-        // Todo refaktoori j2rgnev kood funktsiooniks mis v6rdleb requestis olevaid v2ljanimesid andmebaasis olevate v2ljanimedega ja tagastab ainult yhised
-        $set = [
-            'name' => $request->name,
-            'releaseDate' => $request->releaseDate,
-            'description' => $request->description,
-            'genre' => $request->genre,
-            'characters' => $request->characters
-        ];
+
+        // Validate releaseDate
+        //if(!empty($request->releaseYear) && !valid_date($request->releaseYear)){
+         //  output_error(405, "ReleaseYear is not valid");
+       //}
+
+
+        // Define fields allowed for updating
+        $allowed_fields = array_flip( [
+            'name',
+            'releaseYear',
+            'description',
+            'genre',
+            'characters'
+        ]);
+
+
+        // Filter request fields
+        $data = array_intersect_key((array)$request, $allowed_fields);
+
 
         // Update database
-        $query = $this->db->update('game')->set($set)->where('id', $request->id)->execute();
+        $query = $this->db->update('game')->set($data)->where('id', $request->id)->execute();
 
 
-
-        $this->output((array)$query);
+        // Verify that query succeeded
+        if ($query === false) {
+            output_error(500, "Server error");
+        }
+        exit();
     }
 
 
-    function delete()
+    function delete($id)
     {
-        $query = $this->db->deleteFrom('game')->where('id', 4);
+        if (!$id > 0) {
+            output_error(405, "ID is not valid");
+        }
+
+        $movie = $this->db->from("game")->where("id", $id)->fetch();
+        if (!$movie) {
+            output_error(404, "Game not found!");
+        }
+
+        $query = $this->db->deleteFrom('game')->where('id', $id);
         $query->execute();
-        $this->output((array)$query);
+        if ($query === false) {
+            output_error(500, "Server error");
+        }
+        header("HTTP/1.1 204 Ok!");
     }
 
 
